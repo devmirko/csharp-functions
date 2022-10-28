27d9688301e992e9b1e2c314d40e5e6d9b1d2a6b
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-10-28 15:20:19 +0200

    fine esercizio

--- Program.cs
+++ Program.cs
@@ -2,3 +2,66 @@
 Console.WriteLine("Hello, World!");
 
 
+
+
+int[] arrayUser = { 2, 6, 7, 5, 3, 9 };
+//Stampare l’array di numeri fornito a video
+StampaArray(arrayUser);
+
+//Stampare l’array di numeri fornito a video, dove ogni numero è stato prima elevato al quadrato;
+int[] arrayQuadrato = ElevaArrayAlQuadrato(arrayUser);
+StampaArray(arrayQuadrato);
+
+//Stampare la somma di tutti i numeri
+int somma = SommaElementiArray(arrayUser);
+StampaArray(somma);
+
+
+//Stampare la somma di tutti i numeri elevati al quadrati
+int sommaQuadrato = quadrato(somma);
+StampaArray(sommaQuadrato);
+//In questo esercizio vi chiedo di definire qualche funzione di utilità che poi potete usare per poter fare
+//operazioni complesse nei vostri programma principale.
+
+//Scrivete nel vostro programma principale Program.cs le seguenti funzioni di base:
+//void StampaArray
+
+void StampaArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) { 
+        Console.WriteLine(array[i]); 
+    }
+}
+
+int quadrato(int x)
+{
+
+    x *= x;
+
+    return x;
+
+}
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    int[] arrayQuadrato = new int[array.Length];
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayQuadrato = array[i] * array[i];
+       
+    }
+    return arrayQuadrato;
+
+}
+
+int SommaElementiArray(int[] array)
+{
+    int totArray = 0
+    for (int i = 0; i < array.Length; i++)
+    {
+        totArray += array[i]
+    }
+    return totArray;
+}
+
